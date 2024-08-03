776ad73555baab333716d1bfd052919d961caac0
Heetesh.Panghanti <Heetesh.Panghanti@acentra.com>
2024-08-03 22:40:54 +0530

    Added the footer links.

--- portfolioapp/src/components/Footer.tsx
+++ portfolioapp/src/components/Footer.tsx
@@ -8,18 +8,21 @@ const FooterContainer = styled.footer`
   text-align: center;
 `;
 
+const LinkedInStyle = { color: "#0082ca;"};
+const GitHub = { color: "#333333;" };
+const StackOverflow = { color: "#ffac44;" };
+
 const Footer = () => {
     return (
-        <FooterContainer>
-            <p>&copy; 2024 Portfolio. All Rights Reserved.</p>
-            <div>
-                <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
-                    Twitter
-                </a>
-                <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
-                    LinkedIn
-                </a>
-            </div>
+        <FooterContainer className="navbar-fixed-bottom">
+                <div className="container">
+                <div className="mb-4">
+                    <a href="https://www.linkedin.com/in/heetesh-panghanti-94549b117/" target="_blank"><i className="fab fa-linkedin-in fa-2x p-2" style={LinkedInStyle}></i></a>
+                    <a href="https://github.com/hpissei" target="_blank" ><i className="fab fa-github fa-2x p-2" style={GitHub}></i></a>
+                    <a href="https://stackoverflow.com/users/9322797/hp-issei" target="_blank"><i className="fab fa-stack-overflow fa-2x p-2" style={StackOverflow}></i></a>
+                    <p>© 2024 All rights reserved.</p>
+                    </div>
+                </div>
         </FooterContainer>
     );
 };
