c8eaf7b9bf66b0868b0ca124b983d7b3844504fe
Heetesh.Panghanti <Heetesh.Panghanti@acentra.com>
2024-08-07 15:35:26 +0530

    Updates

---
+++ assets/About-FEauGB5I.js
@@ -0,0 +1,29 @@
+import{d as n,j as e,L as r}from"./index-DTU84UeK.js";import{m as o}from"./motion-B-sa-vCL.js";const s=n.section`
+  padding: 4rem 2rem;
+  background-color: #f7f8fa;
+  text-align: center;
+`,t=n.div`
+  max-width: 800px;
+  margin: 0 auto;
+`,a=n.h2`
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+`,l=n.p`
+  font-size: 1.125rem;
+  line-height: 1.6;
+  margin-bottom: 2rem;
+  color: #555;
+`,c=n.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: 2rem;
+`,i=n.div`
+  background: #fff;
+  border-radius: 5px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  padding: 0.75rem 1.5rem;
+  margin: 0.5rem;
+  font-size: 1rem;
+  color: #333;
+`,h=()=>e.jsx(s,{id:"about",children:e.jsxs(t,{children:[e.jsx(a,{children:"About Me"}),e.jsx(l,{children:"I'm a passionate software developer, team lead, project manager & consultant with experience in building native, web and mobile applications. I love creating  user-friendly and efficient solutions that solve real-world problems. My main focus is on developing clean and maintainable code, and I'm always eager to learn new technologies and improve my skills."}),e.jsxs(c,{children:[e.jsx(i,{children:e.jsx(o.div,{whileHover:{scale:1.2,rotate:90},whileTap:{scale:.8,rotate:-90,borderRadius:"100%"},children:".Net"})}),e.jsx(i,{children:".Net Core"}),e.jsx(i,{children:"C#"}),e.jsx(i,{children:"SQL"}),e.jsx(i,{children:"Java"}),e.jsx(i,{children:"JavaScript"}),e.jsx(i,{children:"TypeScript"}),e.jsx(i,{children:"React"}),e.jsx(i,{children:"Node.js"}),e.jsx(i,{children:"Python"}),e.jsx(i,{children:"Golang"}),e.jsx(i,{children:"GraphQL"}),e.jsx(i,{children:"Docker"}),e.jsx(i,{children:"gRPC"}),e.jsx(i,{children:"Microservices"}),e.jsx(i,{children:"Azure"}),e.jsx(i,{children:"AWS"}),e.jsx(i,{children:"ElasticSearch"}),e.jsx(i,{children:"CosmosDB"}),e.jsx(i,{children:"REST API"})]}),e.jsx(r,{to:"mailto:hpultimatemedia@email.com",className:"btn btn-lg btn-warning",children:"Get In Touch"})]})});export{h as default};
