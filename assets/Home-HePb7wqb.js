d8b8e70882879db41543f5396aff349294d1d60c
Heetesh.Panghanti <Heetesh.Panghanti@acentra.com>
2024-08-07 15:40:44 +0530

    Updates

--- assets/Home-HePb7wqb.js
+++
@@ -1,8 +0,0 @@
-import{d as n,j as t,N as i}from"./index-DTU84UeK.js";import{m as e}from"./motion-B-sa-vCL.js";const o=n.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-`,a={color:"rgb(182 129 22)"},l=()=>t.jsxs(o,{children:[t.jsx(e.h1,{initial:{opacity:0},animate:{opacity:1},transition:{duration:1},children:t.jsxs("span",{className:"row teko-font teko-font-size-regular",style:a,children:[" Hello,",t.jsx("br",{})," I'm Heetesh Panghanti "]})}),t.jsx(e.p,{initial:{x:"-100vw"},animate:{x:0},transition:{duration:1},children:t.jsx("div",{className:"teko-font teko-font-size-medium mt-n1",children:"A Full Stack Engineer & Consultant"})}),t.jsx(e.p,{initial:{scale:0},animate:{scale:1},transition:{duration:.5},children:t.jsx(i,{to:"/projects",className:"teko-font teko-font-size-small mt-n1",children:"View My Work"})})]});export{l as default};
---
+++ assets/Projects-C5krsh1P.js
@@ -0,0 +1,51 @@
+import{d as t,m as f,j as e}from"./index-B2llxZeK.js";const r=t.section`
+  padding: 4rem 2rem;
+  background-color: #fff;
+  text-align: center;
+`,n=t.div`
+  max-width: 1000px;
+  margin: 0 auto;
+`,s=t.h2`
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+`,a=t.p`
+  font-size: 1.125rem;
+  line-height: 1.6;
+  margin-bottom: 2rem;
+  color: #555;
+`,c=t.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 2rem;
+`,l=t(f.div)`
+  background: #f7f8fa;
+  border-radius: 10px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  padding: 1.5rem;
+  text-align: left;
+  cursor: pointer;
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 0px 8px 12px rgba(0, 0, 0, 0.2);
+  }
+`,d=t.h3`
+  font-size: 1.5rem;
+  margin-bottom: 0.5rem;
+`,h=t.p`
+  font-size: 1rem;
+  color: #777;
+  margin-bottom: 1rem;
+`,p=t.div`
+  font-size: 0.875rem;
+  color: #333;
+`,m=t.a`
+  color: #007bff;
+  text-decoration: none;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`,w=()=>{const g=[{title:"Skillsurvey",description:"SkillSurvey is the leader in online reference checking, sourcing and credentialing, providing immediately useful insights to help employers make better hiring decisions. SkillSurvey speeds hiring for healthcare, commercial, higher education, and staffing and recruiting organizations.",techStack:"",link:"https://www.icims.com/products/talent-cloud-applications/skillsurvey/"},{title:"CCH Axcess",description:"CCH Axcess is the only complete cloud solution, allowing you to transition effortlessly from return preparation, to research, to document exchange and billing.",techStack:"",link:"https://www.wolterskluwer.com/en/solutions/cch-axcess"},{title:"Atrezzo",description:"Atrezzo is a proprietary technology platform that integrates essential care management features and all relevant data into one comprehensive solution. Leading-edge technology coupled with an intuitive user experience provides a foundation for proactive care management.",techStack:"",link:"https://acentra.com/technologies/atrezzo/"},{title:"Energage",description:"Energage Platform, is an suite of HR technology tools that enable companies to improve the working lives of individuals, build distinct brands, and achieve better business results. ",techStack:"",link:"https://www.energage.com/platform/"},{title:"Raptivity",description:"Raptivity is a must-have tool for instructional designers, corporate trainers, and higher-ed professors to create eLearning interactions in a snap. Unlike common authoring tools that offer basic templates, Raptivity has visually engaging and sophisticated interactions. Let Raptivity take care of interaction design, so you can focus on content and curriculum.",techStack:"",link:"https://www.raptivity.com/"},{title:"Quillionz",description:"Quillionz is an online question generator that uses artificial intelligence (AI) to create questions from texts.",techStack:"",link:"https://www.quillionz.com/"}],u=[{title:"Portfolio",description:"A personal website to showcase my portfolio and blog posts.",techStack:"React, Styled-Components, Framer Motion",link:"https://github.com/hpissei/portfolio/"},{title:"Restaurant Reservation CLI",description:"A command-line interface for managing the restaurant reservations.",techStack:"Golang",link:"https://github.com/hpissei/restaurantresrervation-cli/"}];return e.jsxs(e.Fragment,{children:[e.jsx(r,{id:"professionalProjects",children:e.jsxs(n,{children:[e.jsx(s,{children:"Professional Projects"}),e.jsx(a,{children:"I have worked on several products, projects ranging from native, web, mobile & microservices applications. The products on which I worked professionally due to confidentiliaity the source code,the tech stack is not publically shareable. Some professional products are as follows."}),e.jsx(c,{children:g.map((i,o)=>e.jsxs(l,{whileHover:{scale:1.02},whileTap:{scale:.98},children:[e.jsx(d,{children:i.title}),e.jsx(h,{children:i.description}),e.jsx(p,{children:i.techStack}),e.jsx(m,{href:i.link,target:"_blank",rel:"noopener noreferrer",children:"View Product Site"})]},o))})]})}),e.jsx(r,{id:"projects",children:e.jsxs(n,{children:[e.jsx(s,{children:"Personal Projects"}),e.jsx(a,{children:"I have created some personal projects which are as follows."}),e.jsx(c,{children:u.map((i,o)=>e.jsxs(l,{whileHover:{scale:1.02},whileTap:{scale:.98},children:[e.jsx(d,{children:i.title}),e.jsx(h,{children:i.description}),e.jsx(p,{children:i.techStack}),e.jsx(m,{href:i.link,target:"_blank",rel:"noopener noreferrer",children:"View Project Source Code"})]},o))})]})})]})};export{w as default};
